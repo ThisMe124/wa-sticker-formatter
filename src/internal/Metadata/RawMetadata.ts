fddd381a25cc41d9659d8f58d55ebf5597333d29
Strong Heart <88891071+ThisMe124@users.noreply.github.com>
2023-06-12 14:17:46 +0800

    update src/internal/Metadata/RawMetadata.ts

--- src/internal/Metadata/RawMetadata.ts
+++ src/internal/Metadata/RawMetadata.ts
@@ -6,10 +6,14 @@ export default class RawMetadata implements IRawMetadata {
     'sticker-pack-id': string
     'sticker-pack-name': string
     'sticker-pack-publisher': string
+    'android-app-store-link': string
+    'ios-app-store-link': string
     constructor(options: Metadata) {
         this['sticker-pack-id'] = options.id || Utils.generateStickerID()
         this['sticker-pack-name'] = options.pack || ''
         this['sticker-pack-publisher'] = options.author || ''
+        this['android-app-store-link'] = options.androidapp_link || 'https://play.google.com/store/apps/details?id=com.marsvard.stickermakerforwhatsapp'
+        this['ios-app-store-link'] = options.iosapp_link || 'https://itunes.apple.com/app/sticker-maker-studio/id1443326857'
         this.emojis = options.categories || []
     }
 }
